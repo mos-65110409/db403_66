26c300e261b8e3ef1c754cdf595f1443ff329a33
mos-65110409 <65110409@dpu.ac.th>
2023-09-12 10:28:32 +0700

    add dice.php

---
+++ lab3/dice.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dices Roll</title>
+</head>
+<body>
+    <?php 
+    function dice($score = FALSE){
+        $d1 = rand(1,6);
+        $d2 = rand(1,6);
+        return '2 DICES ROLL '.($score? '=>'.$d1.'+'.$d2 : '').' => ผลรวมคือ ' .$d1+$d2;
+    }
+    ?>
+    <h1>Without score</h1>
+    <?php echo dice();?>
+    <h1>With score</h1>
+    <?php echo dice(TRUE);?>
+</body>
+</html>
