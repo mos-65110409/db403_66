1f2c838db0fa9feb7bb1135049df7b97b531bd6b
mos-65110409 <65110409@dpu.ac.th>
2023-09-19 11:24:47 +0700

    Poker.php

---
+++ lab4/poker.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poker</title>
+    <style>
+       .hearts , .diams{
+        color: red;
+       } 
+       .spades , .clubs {
+        color: black;
+       }
+    </style>
+</head>
+<body>
+    <?php 
+    $suits = ['spades','clubs','hearts','diams'];
+    $ranks = explode(',','A,2,3,4,5,6,7,8,9,10,J,Q,K');
+    $decks = [];
+    foreach ($suits as $suit) {
+        foreach ($ranks as $rank){
+            $deck[] = ['suit'=>$suit, 'rank' =>$rank ];
+        }
+    }
+       function deal($deck){
+            
+            $tmp = rand(0,51);
+            $card1 = $deck[$tmp];
+           $cardA =  "
+           <span class='{$card1['suit']}'>
+            {$card1['rank']}&{$card1['suit']};
+            </span>";
+            unset($deck[$tmp]);
+            sort($deck);
+            $card2 =$deck[rand(0,50)];
+            $cardB = "
+            <span class='{$card2['suit']}'>
+            {$card2['rank']}&{$card2['suit']};
+            </span>";
+            return $cardA. '+' .$cardB;
+        }
+    ?>
+    <h1>ไพ่ที่ได้</h1>
+    <h1><?php
+    echo deal($deck);
+    
+    ?></h1>
+ 
+</body>
+</html>
